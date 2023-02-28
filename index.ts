ebdc1f09ef456f482e73e01b5306032e5d52bd9a
XPoweR29 <xpower-29@wp.pl>
2023-02-28 19:02:05 +0100

    Express configuration

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+const app = express();
+//cors
+app.use(express.json());
+//rateLimiter
+
+//routers
+
+//handleError
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on http://localhost:3001');
+});
