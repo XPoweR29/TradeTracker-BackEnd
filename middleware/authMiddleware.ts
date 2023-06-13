20513dfc861add525f2c125fc66d407c385a5596
XPoweR29 <xpower-29@wp.pl>
2023-06-13 09:30:23 +0200

    Change exceptions handling in authMiddleware

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -1,24 +1,31 @@
 import { NextFunction, Request, Response } from "express";
 import * as jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
+import { RequestWithUserObj } from "../types";
+import { UserRecord } from "../records/user.record";
 
 dotenv.config();
 
-export const authMiddleware = (req: Record<'user', any> & Request, res: Response, next: NextFunction) => {
-    const token = req.cookies?.jwt;
+export const authMiddleware = (req: RequestWithUserObj, res: Response, next: NextFunction) => {
+    const token = req.cookies?.jwt; 
     if(!token) {
         return res.status(401).json({
             message: 'Nieudana autoryzacja'
         });
     }
 
-    jwt.verify(token, process.env.SIGNATURE, (err: Error, data: any) => {
+    jwt.verify(token, process.env.SIGNATURE, async(err: Error, data: any) => {
         if(err) {
-            return res.status(403).json({
-                message: 'Nieudana autoryzacja'
-            });
+            return res.status(403).json({message: 'Nieudana autoryzacja'});
         }
-        req.user = data;
+
+        const user = await UserRecord.getByTokenId(data.tokenId);
+        if(!user) {
+            return res.status(403).json({message: 'Nieudana autoryzacja'});
+        }
+
+        req.user = user;
         next();
+
     });
 };
